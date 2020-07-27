f2c7c58aecdf4c6b4b3c57e14d97285e92bbed76
latte0119 <latte.malta@gmail.com>
2020-07-28 00:10:35 +0900

    mainjp

--- weather.go
+++ weather.go
@@ -1,5 +1,7 @@
 package owmjp
 
+import "errors"
+
 type Weather struct {
 	Coord struct {
 		Lat float64 `json:"lat"`
@@ -40,4 +42,48 @@ type Weather struct {
 	DtTxt string `json:"dt_txt"`
 	ID    int    `json:"id"`
 	Name  string `json:"name"`
+}
+
+func (w Weather) MainJP() (string, error) {
+	id := w.ID
+	if id == 0 {
+		return "", errors.New("Error : ID is undefined")
+	}
+
+	switch {
+	case 200 <= id && id < 300:
+		return "雷雨", nil
+	case 300 <= id && id < 400:
+		return "霧雨", nil
+	case 400 <= id && id < 500:
+		return "雨", nil
+	case 500 <= id && id < 600:
+		return "雪", nil
+	case id == 701:
+		return "靄", nil
+	case id == 711:
+		return "煙", nil
+	case id == 721:
+		return "霞", nil
+	case id == 731:
+		return "砂塵", nil
+	case id == 741:
+		return "霧", nil
+	case id == 751:
+		return "砂あらし", nil
+	case id == 761:
+		return "砂塵", nil
+	case id == 762:
+		return "灰", nil
+	case id == 771:
+		return "スコール", nil
+	case id == 781:
+		return "竜巻", nil
+	case id == 800:
+		return "快晴", nil
+	case 800 < id:
+		return "曇り", nil
+	default:
+		return "", errors.New("Error : ID is invalid")
+	}
 }
